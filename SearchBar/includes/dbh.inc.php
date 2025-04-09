e1afb76278c1fa25b5a0441e273edd9f8c0f19f2
oguz00000 <bikmazoguz2000@gmail.com>
2025-04-09 12:46:25 +0300

    Files deleted

--- SearchBar/includes/dbh.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbusername = "root";
-
-try {
-    $pdo = new PDO("mysql:host=localhost;dbname=testdb", $dbusername);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
